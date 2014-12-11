2a312d163a23651a1db3a1d44264230157feb683
Jared Meeker <jaredm@morinda.com>
2014-12-10 20:44:21 -0700

    Version bump to 2.3.0

--- src/morinda.com/evh/main.go
+++ src/morinda.com/evh/main.go
@@ -39,7 +39,7 @@ var SiteDown bool
 var Templates *template.Template
 
 // Constants
-const VERSION = "2.2.3"
+const VERSION = "2.3.0"
 const TimeLayout = "Jan 2, 2006 at 3:04pm (MST)"
 
 func init() {
