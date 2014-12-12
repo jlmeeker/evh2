dff92c09436698bcf584e9550c21b8c302e0ac06
Jared Meeker <jaredm@morinda.com>
2014-12-11 19:58:54 -0700

    Added systemd service file to package.sh script

--- package.sh
+++ package.sh
@@ -4,4 +4,4 @@
 VERSION=`grep VERSION src/morinda.com/evh/main.go | cut -d "\"" -f 2`
 
 # Tar up the app
-./build.sh && cd ../ && tar --totals -zcvf evh2-$VERSION.tar.gz evh2/LICENSE evh2/README.md evh2/sample-config.gcfg evh2/apache.howto evh2/tmpl evh2/bin/
+./build.sh && cd ../ && tar --totals -zcvf evh2-$VERSION.tar.gz evh2/LICENSE evh2/README.md evh2/evh2.service evh2/sample-config.gcfg evh2/apache.howto evh2/tmpl evh2/bin/
